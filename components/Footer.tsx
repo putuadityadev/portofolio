d495133fcb44deef4003ac591a96318ac8e91d13
aditya <adityamph1@gmail.com>
2024-12-31 18:11:05 +0800

    first

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -37,16 +37,14 @@ const Footer = () => {
 
         <div className="flex items-center md:gap-3 gap-6">
           {socialMedia.map((profile) => (
-            <a href={profile.link} key={profile.id} target='_blank'>
-              <div className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300">
-                <img 
-                  src={profile.img}
-                  alt={profile.id}
-                  width={20}
-                  height={20}
-                />
-              </div>
-            </a>
+            <div className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300">
+              <img 
+                src={profile.img}
+                alt={profile.id}
+                width={20}
+                height={20}
+              />
+            </div>
           ))}
         </div>
       </div>
